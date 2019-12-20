b254856411b13bd49a82ce2e61b4e3ba08d92a16
Christopher Aguirre <christopher.aguirre210@gmail.com>
2019-12-20 13:49:39 -0600

    subqueries exercises

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -10,7 +10,7 @@ SELECT * FROM employees WHERE (first_name = ('Irena') OR first_name = ('Vidya') 
 SELECT * FROM employees WHERE last_name LIKE '%E' OR last_name LIKE 'E%';
 SELECT * FROM employees WHERE last_name LIKE '%E' AND last_name LIKE 'E%';
 SELECT * FROM employees WHERE (hire_date BETWEEN '1990-01-01' AND '1999-12-31') AND birth_date LIKE  '%12-25' ORDER BY first_name DESC, last_name ASC;
-SELECT * FROM employees WHERE (last_name LIKE '%q%') AND NOT '%qu%';
+SELECT * FROM employees WHERE (last_name LIKE '%q%') NOT LIKE '%qu%';
 
 
 
---
+++ subqueries_exercises.sql
@@ -0,0 +1,24 @@
+USE employees;
+
+
+SELECT * FROM employees
+WHERE hire_date = (
+SELECT hire_date
+    FROM employees
+    WHERE emp_no = '101010'
+);
+
+SELECT title FROM titles
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM employees
+    WHERE first_name = 'Aamod'
+);
+
+SELECT first_name, last_name FROM employees
+WHERE gender = 'F'
+AND emp_no In (
+    SELECT emp_no FROM dept_manager
+    WHERE to_date > NOW()
+);
+
