6eddcc466a69f94e82fa992cf59dcdfcfea8c8e5
Christopher Aguirre <christopher.aguirre210@gmail.com>
2019-12-24 12:06:41 -0600

    limit exercise complete

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,16 +1,18 @@
 USE employees;
-SELECT first_name,last_name FROM employees WHERE first_name IN ('Irena', 'Vidya','Maya') ORDER BY last_name,first_name DESC;
-SELECT * FROM employees WHERE last_name LIKE 'E%' ORDER BY emp_no DESC;
-SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
-SELECT * FROM employees WHERE birth_date LIKE  '%12-25';
-SELECT * FROM employees WHERE last_name LIKE '%q%';
+#2
+SELECT first_name,last_name FROM employees WHERE first_name IN ('Irena', 'Vidya','Maya') ORDER BY first_name;
 
-SELECT * FROM employees WHERE (first_name = ('Irena') OR first_name = ('Vidya') OR first_name = ('Maya'))
- AND gender = 'M';
-SELECT * FROM employees WHERE last_name LIKE '%E' OR last_name LIKE 'E%';
-SELECT * FROM employees WHERE last_name LIKE '%E' AND last_name LIKE 'E%';
-SELECT * FROM employees WHERE (hire_date BETWEEN '1990-01-01' AND '1999-12-31') AND birth_date LIKE  '%12-25' ORDER BY birth_date ASC, hire_date DESC;
-SELECT * FROM employees WHERE (last_name LIKE '%q%') NOT LIKE '%qu%';
+#3
+SELECT first_name,last_name FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name,last_name;
+
+#4
+SELECT first_name,last_name FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY last_name,first_name;
+
+#5
+SELECT * FROM employees WHERE last_name LIKE '%E%' ORDER BY emp_no DESC;
+
+#7
+SELECT * FROM employees WHERE (hire_date BETWEEN '1990-01-01' AND '1999-12-31') AND birth_date LIKE  '%12-25' ORDER BY birth_date ASC, hire_date DESC LIMIT 1;
 
 
 
