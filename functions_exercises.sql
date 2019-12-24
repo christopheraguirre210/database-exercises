0a3c633717f123869468dd510e0c7b25138ac3bd
Christopher Aguirre <christopher.aguirre210@gmail.com>
2019-12-24 13:11:35 -0600

    functions exercise

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,21 +1,32 @@
 USE employees;
-SELECT first_name,last_name FROM employees WHERE first_name IN ('Irena', 'Vidya','Maya') ORDER BY last_name,first_name DESC;
-SELECT * FROM employees WHERE last_name LIKE 'E%' ORDER BY emp_no DESC;
-SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
-SELECT * FROM employees WHERE birth_date LIKE  '%12-25';
-SELECT * FROM employees WHERE last_name LIKE '%q%';
 
-SELECT * FROM employees WHERE (first_name = ('Irena') OR first_name = ('Vidya') OR first_name = ('Maya'))
- AND gender = 'M';
-SELECT * FROM employees WHERE last_name LIKE '%E' OR last_name LIKE 'E%';
-SELECT * FROM employees WHERE last_name LIKE '%E' AND last_name LIKE 'E%';
-SELECT * FROM employees WHERE (hire_date BETWEEN '1990-01-01' AND '1999-12-31') AND birth_date LIKE  '%12-25' ORDER BY first_name DESC, last_name ASC;
-SELECT * FROM employees WHERE (last_name LIKE '%q%') NOT LIKE '%qu%';
+#2
+SELECT CONCAT(first_name, ' ', last_name) AS full_name FROM employees WHERE(last_name LIKE 'e%' AND last_name LIKE '%e') ORDER BY emp_no DESC;
 
-SELECT CONCAT(first_name, ' ', last_name) FROM emplyees WHERE first_name IN ('Irena', 'Vidya', 'Maya') AND gender = 'M';
+#3
+SELECT * FROM employees WHERE (hire_date BETWEEN '1990-01-01' AND '1999-12-31') AND birth_date LIKE  '%12-25' ORDER BY datediff(hire_date,NOW());
+
+SELECT first_name,last_name,(datediff(NOW(), hire_date)) AS days_since_hired
+FROM employees WHERE (hire_date BETWEEN '1990-01-01' AND '1999-12-31') AND birth_date LIKE '%12-25'
+ORDER BY birth_date ASC,hire_date DESC LIMIT 25;
 
 
-SELECT *, dateddiff(now(), hire_date) FROM
+
+
+
+
+
+
+
+
+#SELECT *, dateddiff(now(), hire_date) FROM
+
+SELECT CONCAT('Hello ', 'Codeup', '!');
+
+SELECT first_name FROM employees
+WHERE first_name NOT LIKE '%a%';
+
+SELECT NOW();
 
 
 
--- limit_exercises.sql
+++ limit_exercises.sql
@@ -1,7 +1,10 @@
 SELECT DISTINCT title FROM titles;
 
+#2
 SELECT DISTINCT last_name FROM employees ORDER BY last_name DESC LIMIT 10;
 
+#3
 SELECT first_name,last_name FROM employees WHERE (hire_date BETWEEN '1990-01-01' AND '1999-12-31') AND birth_date LIKE  '%12-25' ORDER BY birth_date ASC, hire_date DESC LIMIT 5;
 
+#4
 SELECT first_name,last_name FROM employees WHERE (hire_date BETWEEN '1990-01-01' AND '1999-12-31') AND birth_date LIKE  '%12-25' ORDER BY birth_date ASC, hire_date DESC LIMIT 5 OFFSET 45;
