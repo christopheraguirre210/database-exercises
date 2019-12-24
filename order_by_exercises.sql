59ecc7d8bc59bb65be3be8cb0ccbbd84819602c1
Christopher Aguirre <christopher.aguirre210@gmail.com>
2019-12-24 11:48:18 -0600

    limit and order by exercise update

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -9,7 +9,7 @@ SELECT * FROM employees WHERE (first_name = ('Irena') OR first_name = ('Vidya') 
  AND gender = 'M';
 SELECT * FROM employees WHERE last_name LIKE '%E' OR last_name LIKE 'E%';
 SELECT * FROM employees WHERE last_name LIKE '%E' AND last_name LIKE 'E%';
-SELECT * FROM employees WHERE (hire_date BETWEEN '1990-01-01' AND '1999-12-31') AND birth_date LIKE  '%12-25' ORDER BY first_name DESC, last_name ASC;
+SELECT * FROM employees WHERE (hire_date BETWEEN '1990-01-01' AND '1999-12-31') AND birth_date LIKE  '%12-25' ORDER BY birth_date ASC, hire_date DESC;
 SELECT * FROM employees WHERE (last_name LIKE '%q%') NOT LIKE '%qu%';
 
 
