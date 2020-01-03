10edf87b10fc5aa135c2767abf7367fa09d40321
Christopher Aguirre <christopher.aguirre210@gmail.com>
2020-01-03 10:31:43 -0600

    a migration file that created a table called people with all the specs

--- christmas_migration.sql
+++ christmas_migration.sql
@@ -1,4 +1,20 @@
 USE christmas_db;
-
+DROP TABLE IF EXISTS people;
+CREATE TABLE people(
 first_name VARCHAR(50) NOT NULL,
 last_name VARCHAR(50) NOT NULL,
+age INT NOT NULL,
+birthday DATE,
+nice TINYINT,
+wishlist VARCHAR(150) NOT NULL
+);
+
+
+
+
+
+
+
+
+
+-- mysql -u USERNAME -p -t < filename.sql (run this in the terminal)
