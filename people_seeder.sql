aec68e5a05a252006b5446284d3feb335344cb9f
Christopher Aguirre <christopher.aguirre210@gmail.com>
2020-01-04 14:48:08 -0600

    created a seeder script for the people table and tested to make sure it worked

---
+++ people_seeder.sql
@@ -0,0 +1,21 @@
+USE christmas_db;
+INSERT INTO people(first_name, last_name, age, birthday, nice, wishlist)
+VALUES('Christopher', 'Aguirre', 30, '1989-07-02', 1, 'Passing grades');
+INSERT INTO people(first_name, last_name, age, birthday, nice, wishlist)
+VALUES('Jacob', 'Barrow', 30, '1988-12-25', 1, 'Passing grades');
+INSERT INTO people(first_name, last_name, age, birthday, nice, wishlist)
+VALUES('Bryan', 'Walsh', 30, '1988-12-25', 1, 'Passing grades');
+INSERT INTO people(first_name, last_name, age, birthday, nice, wishlist)
+VALUES('Andrew', 'Swint', 30, '1988-12-25', 1, 'Passing grades');
+INSERT INTO people(first_name, last_name, age, birthday, nice, wishlist)
+VALUES('JW', 'Hester', 30, '1988-12-25', 1, 'Passing grades');
+INSERT INTO people(first_name, last_name, age, birthday, nice, wishlist)
+VALUES('Nico', 'Virgino', 30, '1988-12-25', 1, 'Passing grades');
+INSERT INTO people(first_name, last_name, age, birthday, nice, wishlist)
+VALUES('Roger', 'Mayer', 30, '1988-12-25', 1, 'Passing grades');
+INSERT INTO people(first_name, last_name, age, birthday, nice, wishlist)
+VALUES('Ethan', 'Joiner', 30, '1988-12-25', 1, 'Passing grades');
+INSERT INTO people(first_name, last_name, age, birthday, nice, wishlist)
+VALUES('Kenneth', 'Howell', 30, '1988-12-25', 1, 'Passing grades');
+INSERT INTO people(first_name, last_name, age, birthday, nice, wishlist)
+VALUES('Colin', 'Marchbanks', 30, '1988-12-25', 1, 'Passing grades');
