2d255ce30ef707ec66a8ea0f5535c470955d6f97
Christopher Aguirre <christopher.aguirre210@gmail.com>
2020-01-04 14:54:15 -0600

    dropped the table and added more information to see if the functionality was working correctly

--- people_seeder.sql
+++ people_seeder.sql
@@ -2,20 +2,20 @@ USE christmas_db;
 INSERT INTO people(first_name, last_name, age, birthday, nice, wishlist)
 VALUES('Christopher', 'Aguirre', 30, '1989-07-02', 1, 'Passing grades');
 INSERT INTO people(first_name, last_name, age, birthday, nice, wishlist)
-VALUES('Jacob', 'Barrow', 30, '1988-12-25', 1, 'Passing grades');
+VALUES('Jacob', 'Barrow', 31, '1988-12-25', 1, 'Passing grades');
 INSERT INTO people(first_name, last_name, age, birthday, nice, wishlist)
-VALUES('Bryan', 'Walsh', 30, '1988-12-25', 1, 'Passing grades');
+VALUES('Bryan', 'Walsh', 31, '1988-12-25', 1, 'Passing grades');
 INSERT INTO people(first_name, last_name, age, birthday, nice, wishlist)
-VALUES('Andrew', 'Swint', 30, '1988-12-25', 1, 'Passing grades');
+VALUES('Andrew', 'Swint', 31, '1988-12-25', 1, 'Passing grades');
 INSERT INTO people(first_name, last_name, age, birthday, nice, wishlist)
-VALUES('JW', 'Hester', 30, '1988-12-25', 1, 'Passing grades');
+VALUES('JW', 'Hester', 31, '1988-12-25', 1, 'Passing grades');
 INSERT INTO people(first_name, last_name, age, birthday, nice, wishlist)
-VALUES('Nico', 'Virgino', 30, '1988-12-25', 1, 'Passing grades');
+VALUES('Nico', 'Virgino', 31, '1988-12-25', 1, 'Passing grades');
 INSERT INTO people(first_name, last_name, age, birthday, nice, wishlist)
-VALUES('Roger', 'Mayer', 30, '1988-12-25', 1, 'Passing grades');
+VALUES('Roger', 'Mayer', 31, '1988-12-25', 1, 'Passing grades');
 INSERT INTO people(first_name, last_name, age, birthday, nice, wishlist)
-VALUES('Ethan', 'Joiner', 30, '1988-12-25', 1, 'Passing grades');
+VALUES('Ethan', 'Joiner', 31, '1988-12-25', 1, 'Passing grades');
 INSERT INTO people(first_name, last_name, age, birthday, nice, wishlist)
-VALUES('Kenneth', 'Howell', 30, '1988-12-25', 1, 'Passing grades');
+VALUES('Kenneth', 'Howell', 31, '1988-12-25', 1, 'Passing grades');
 INSERT INTO people(first_name, last_name, age, birthday, nice, wishlist)
-VALUES('Colin', 'Marchbanks', 30, '1988-12-25', 1, 'Passing grades');
+VALUES('Colin', 'Marchbanks', 31, '1988-12-25', 1, 'Passing grades');
