53a39c752254e1355b352b4af8e79144a2780e8b
Christopher Aguirre <christopher.aguirre210@gmail.com>
2020-01-06 13:18:03 -0600

    changes for readability

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,7 +1,9 @@
 USE employees;
 
 #2
-SELECT CONCAT(first_name, ' ', last_name) AS full_name FROM employees WHERE(last_name LIKE 'e%' AND last_name LIKE '%e') ORDER BY emp_no DESC;
+SELECT CONCAT(first_name, ' ', last_name) AS full_name
+FROM employees WHERE(last_name LIKE 'e%' AND last_name LIKE '%e')
+ORDER BY emp_no DESC;
 
 #3
 SELECT first_name,last_name,(datediff(NOW(), hire_date)) AS days_since_hired
